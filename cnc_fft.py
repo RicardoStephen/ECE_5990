41a0842df409f095579bf412ced1f80aa66ce849
Jay Fetter <jdf258@cornell.edu>
2016-05-15 15:45:35 -0400

    FFT Stuff. Getting started.

---
+++ cnc_fft.py
@@ -0,0 +1,36 @@
+#http://glowingpython.blogspot.ro/2011/08/how-to-plot-frequency-spectrum-with.html
+from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
+from scipy import fft, arange, ifft
+from numpy import sin, linspace, pi
+from scipy.io.wavfile import read,write
+
+def plotSpectru(y,Fs):
+ n = len(y) # lungime semnal
+ k = arange(n)
+ T = n/Fs
+ frq = k/T # two sides frequency range
+ frq = frq[range(n/2)] # one side frequency range
+
+ Y = fft(y)/n # fft computing and normalization
+ Y = Y[range(n/2)]
+ 
+ plot(frq,abs(Y),'r') # plotting the spectrum
+ xlabel('Freq (Hz)')
+ ylabel('|Y(freq)|')
+
+
+Fs,data=read('smaller_cnc_wav.wav')
+for 
+
+y=data[:,1]
+lungime=len(y)
+timp=len(y)/Fs
+t=linspace(0,timp,len(y))
+
+subplot(2,1,1)
+plot(t,y)
+xlabel('Time')
+ylabel('Amplitude')
+subplot(2,1,2)
+plotSpectru(y,Fs)
+show()
