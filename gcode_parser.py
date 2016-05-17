9cc464b8511e0d347067b0375873fd9090234d4d
RicardoStephen <rls454@cornell.edu>
2016-05-16 23:10:29 -0400

    added 3d gcode buggy

--- gcode_parser.py
+++ gcode_parser.py
@@ -10,7 +10,10 @@ import sys, fileinput, math, re
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.animation as animation
 from matplotlib import cm, colors, patches
+
+
 prevX          = float(0.0)
 prevY          = float(0.0)
 prevZ          = float(0.0)
@@ -325,6 +328,53 @@ def parseFile(fileName):
             prevY = globalY
             prevZ = globalZ
 
+        def Gen_Line(s, e):
+            length = 50.0
+            stepx = (x2-x1)/(length-1)
+            stepy = (y2-y1)/(length-1)
+            stepz = (z2-z1)/(length-1)
+            lineData = np.empty((3, int(length)))
+            for index in xrange(int(length)):
+                lineData[:, index] = (x1+index*stepx, y1+index*stepy, z1+index*stepz)
+            return lineData
+
+        def update_lines(num, dataLines, lines):
+            for line, data in zip(lines, dataLines):
+                # NOTE: there is no .set_data() for 3 dim data...
+                line.set_data(data[0:2, :num])
+                line.set_3d_properties(data[2, :num])
+            return lines
+
+        fig = plt.figure()
+        ax = p3.Axes3D(fig)
+
+        line_collection = [Gen_Line(s,e) for s,e in lines]
+
+        # Creating fifty line objects.
+        # NOTE: Can't pass empty arrays into 3d version of plot()
+        lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in line_collection]
+        # ax.set_xlim3d([0.0, 1.0])
+        ax.set_xlabel('X')
+        # ax.set_ylim3d([0.0, 1.0])
+        ax.set_ylabel('Y')
+        # ax.set_zlim3d([0.0, 1.0])
+        ax.set_zlabel('Z')
+        ax.set_title('3D Test')
+
+        line_ani = animation.FunctionAnimation(fig, update_lines, 25, fargs=(data, lines), interval=50, blit=False)
+
+        plt.show()
+
+
+
+
+
+
+
+        # for ((x1, y1, z1), (x2, y2, z2)), index in zip(lines, numpy.arange(len(lines))):
+        #     lines[:, index] =
+
+
 
 
         #print("Total Time: %f") % total_time
@@ -342,12 +392,77 @@ def parseFile(fileName):
             (x2, y2, z2) = e
             print("Start: (%f, %f, %f)\t\t|\t\t End:(%f, %f, %f)") % (x1, y1, z1, x2, y2, z2)
         '''
-        plt.autoscale(True, True, True)
-        plt.axis('scaled')
-        plt.show()
+        # plt.autoscale(True, True, True)
+        # plt.axis('scaled')
+        # plt.show()
             #CCW Arc: I,J are centerpoints, globalX, globalY are endpoints
 parseFile(str(sys.argv[1]))
 
 #turtle.done()
 
 
+"""
+A simple example of an animated plot... In 3D!
+"""
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import mpl_toolkits.mplot3d.axes3d as p3
+# import matplotlib.animation as animation
+# import time
+
+
+# def Gen_RandLine(length, dims=2):
+#     """
+#     Create a line using a random walk algorithm
+
+#     length is the number of points for the line.
+#     dims is the number of dimensions the line has.
+#     """
+#     lineData = np.empty((dims, length))
+#     lineData[:, 0] = np.random.rand(dims)
+#     for index in range(1, length):
+#         # scaling the random numbers by 0.1 so
+#         # movement is small compared to position.
+#         # subtraction by 0.5 is to change the range to [-0.5, 0.5]
+#         # to allow a line to move backwards.
+#         step = ((np.random.rand(dims) - 0.5) * 0.1)
+#         lineData[:, index] = lineData[:, index - 1] + step
+
+#     return lineData
+
+
+# def update_lines(num, dataLines, lines):
+#     for line, data in zip(lines, dataLines):
+#         # NOTE: there is no .set_data() for 3 dim data...
+#         line.set_data(data[0:2, :num])
+#         line.set_3d_properties(data[2, :num])
+#     return lines
+
+# # Attaching 3D axis to the figure
+# fig = plt.figure()
+# ax = p3.Axes3D(fig)
+
+# # Fifty lines of random 3-D lines
+# data = [Gen_RandLine(25, 3) for index in range(50)]
+
+# # Creating fifty line objects.
+# # NOTE: Can't pass empty arrays into 3d version of plot()
+# lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
+
+# # Setting the axes properties
+# ax.set_xlim3d([0.0, 1.0])
+# ax.set_xlabel('X')
+
+# ax.set_ylim3d([0.0, 1.0])
+# ax.set_ylabel('Y')
+
+# ax.set_zlim3d([0.0, 1.0])
+# ax.set_zlabel('Z')
+
+# ax.set_title('3D Test')
+
+# # Creating the Animation object
+# line_ani = animation.FuncAnimation(fig, update_lines, 25, fargs=(data, lines),
+#                                    interval=50, blit=False)
+
+# plt.show()
