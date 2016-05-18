7eeb9efcca02cea4c1c352bb2285d52b02930163
RicardoStephen <rls454@cornell.edu>
2016-05-18 14:09:42 -0400

    fixed specgram

--- spectrogram.py
+++ spectrogram.py
@@ -1,10 +1,11 @@
 # http://dsp.stackexchange.com/questions/10743/generating-spectrograms-in-python-with-less-noise
 
-import scipy.io
+import sys
+from scipy.io import wavfile
 import numpy as np
 import pylab
 
-fs,data=wavfile.read(str(sys.argv[1]))
+fs,data = wavfile.read(str(sys.argv[1]))
 channels = [np.array(data[:, 0]), np.array(data[:,1])]
 
 Pxx, f, t, plot = pylab.specgram(
@@ -14,14 +15,14 @@ Pxx, f, t, plot = pylab.specgram(
  detrend=pylab.detrend_none,
  window=pylab.window_hanning,
  noverlap=int(4096*0.5))
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.title('Spectrogram of CNC Noise')
-plt.show()
+pylab.ylabel('Frequency [Hz]')
+pylab.xlabel('Time [sec]')
+pylab.title('Spectrogram of CNC Noise')
+pylab.show()
 
 while True:
  try:
   continue
  except KeyboardInterrupt:
-  plt.close('all')
+  pylab.close('all')
   exit()
