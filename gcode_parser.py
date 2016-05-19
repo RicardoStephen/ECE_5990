ec8dd6df32fae127b43ca17b7b439b7d1a2a990f
Jay F <jdf258@cornell.edu>
2016-05-19 13:14:02 -0400

    Cleaned up gcode_parser

--- gcode_parser.py
+++ gcode_parser.py
@@ -30,11 +30,11 @@ toolNum        = 19
 toolSize       = 0
 rapid          = 100
 toolChangeTime = 5/60
+
 def parseM(line, index):
     index += 1
     return index
     '''
-    #print("Found a valid G cmd: %s. Index: %d\n") % (line, index)
     if(not line[index][0] == 'M'):
         exit(1)
     index += 1
@@ -48,7 +48,6 @@ def parseX(line, index):
         exit(1)
     prevX = globalX
     globalX = float(line[index][1:])
-    #print("Parsing X: %f\n") % globalX
     index += 1
     return index
 
@@ -58,7 +57,6 @@ def parseY(line, index):
         exit(1)
     prevY = globalY
     globalY = float(line[index][1:])
-    #print("Parsing Y: %f\n") % globalY
     index += 1
     return index
 
@@ -68,7 +66,6 @@ def parseZ(line, index):
         exit(1)
     prevZ = globalZ
     globalZ = float(line[index][1:])
-    #print("Parsing Z: %f\n") % globalZ
     index += 1
     return index
 
@@ -77,27 +74,24 @@ def parseI(line, index):
     if(not line[index][0] == 'I'):
         exit(1)
     globalI = float(line[index][1:])
-    #print("Parsing Z: %f\n") % globalZ
     index += 1
     return index
+
 def parseJ(line, index):
     global globalJ
     if(not line[index][0] == 'J'):
         exit(1)
     globalJ = float(line[index][1:])
-    #print("Parsing Z: %f\n") % globalZ
     index += 1
     return index
 
 def parseG0(line, index):
-    #print("Parsing a g0. Here is the line:%s. Index: %d\n") % (line, index)
     while(index < len(line) and not (line[index] in validCmds)):
         index = validDirections[line[index][0]](line, index)
     return index
 
 
 def parseG1(line, index):
-    #print("Parsing a g1. Here is the line:%s. Index: %d\n") % (line, index)
     while(index < len(line) and not (line[index][0] in validCmds)):
         index = validDirections[line[index][0]](line, index)
     return index
@@ -107,6 +101,7 @@ def parseG2(line, index):
     while(index < len(line) and not (line[index][0] in validCmds)):
         index = validDirections[line[index][0]](line, index)
     return index
+
 #Circular Move CW: IJ=centerpoint, XY = endpoints
 def parseG3(line, index):
     while(index < len(line) and not (line[index][0] in validCmds)):
@@ -115,7 +110,6 @@ def parseG3(line, index):
 
 def parseG(line, index):
     global movementType
-    #print("Found a valid G cmd: %s. Index: %d\n") % (line, index)
     if(not line[index][0] == 'G'):
         exit(1)
     index += 1
@@ -135,10 +129,10 @@ def parseT(line, index):
         toolNum = int(line[index][1:])
         index += 1
     return index
+
 #assume its in the format for now: S#####
 def parseSpindle(line, index):
     global spindleSpeed
-    #print("Found a valid spindle: Here is the line: %s\n") % line
     #If this happens, throw an error
     if(not line[index][0] == 'S'):
         exit(1)
@@ -148,12 +142,10 @@ def parseSpindle(line, index):
     else:
         spindleSpeed = int(line[index][1:])
         index += 1
-    #print("Here is the new spindleSpeed: %s\n") % spindleSpeed
     return index
 
 def parseFeed(line, index):
     global feedRate
-    #print("Found a valid feedRate: Here is the line: %s\n") % line
     if(not line[index][0] == 'F'):
         exit(1)
     if(len(line[index]) == 1):
@@ -165,12 +157,10 @@ def parseFeed(line, index):
     return index
 
 def parseComment(line, index):
-    #print("Found a comment: %s\n") % line[index:]
     i = index
     while ( i < len(line)):
         i += 1
         if ')' in line[i-1]:
-            #print("Found end of the comment: %s\n") % line[i-1]
             return i
 validCmds       = {'G':parseG, 'M':parseM, 'T':parseT, 'S':parseSpindle, 'F':parseFeed,
             '(': parseComment}
@@ -195,11 +185,8 @@ movementType = ''
 def parseLine(line):
     i = 0
     while( i < len(line)):
-        #print("This is the index Value: %d\n") % i
         a = str(line[i][0]).split()[0]
-        #print("a: %s\n") % a
         if( a in validCmds):
-            #print("Is a valid cmd: %s\n") % a
             i = validCmds[a](line,i)
         elif( a[0] in validDirections and re.match(r"^([-]?\d+\.?\d+)$",line[i][1:]) is not None):
             i = validGCmds[movementType](line,i)
@@ -214,11 +201,12 @@ def calc_line(x1, y1, x2, y2):
         slope = (y2-y1)/(x2-x1)
     intercept = y2 - slope*x2
     return (slope, intercept)
+
 #returns the distance between point 1 and point 2
 def calcDistance(x1, y1, z1, x2, y2, z2):
     global plot_scale
     return math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
-    #return math.hypot(plot_scale*(x2 - x1),plot_scale*(y2 - y1))
+
 def calcTime(distance, feedRate):
     if(feedRate == 0):
         return 0
@@ -236,28 +224,23 @@ def point_over(point, line):
     else:
         return point[0] > intercept
 def law_of_cosines(A_X, A_Y, B_X, B_Y, C_X, C_Y):
-    #print("A: (%f,%f)\tB: (%f,%f)\tC: (%f,%f)") % (A_X, A_Y, B_X, B_Y, C_X, C_Y)
     #CNC doesn't do 3D arcs
     a = calcDistance(B_X, B_Y, 0, C_X, C_Y, 0)
     b = calcDistance(A_X, A_Y, 0, C_X, C_Y, 0)
     c = calcDistance(A_X, A_Y, 0, B_X, B_Y, 0)
-    #return cosine^-1(b^2+c^2-a^2/(2bc))
-    #print("A: %f B: %f C:%f") %(a,b,c)
     angle = math.acos((b*b+ b*b - a*a)/(2*b*b))
     if(point_over([B_X, B_Y], calc_line(C_X, C_Y, A_X, A_Y))):
-        #print("The point is over the line!!!!!!!\n\n")
         angle = 2*math.pi-angle
-    #print("The Angle returned is: %f") %(angle)
     return (angle,angle*b)
+
 def drawArc():
     global  globalX,  globalY, globalZ, globalI, globalJ, prevX, prevY
-    #print("globalX: %f,  globalY:%f, globalZ:%f, globalI:%f, globalJ:%f, prevX:%f, prevY:%f") % (globalX,  globalY, globalZ, globalI, globalJ, prevX, prevY)
     center = [prevX + globalI, prevY+globalJ]
     radius = calcDistance(globalX, globalY, globalZ, center[0], center[1], prevZ)
     (angle, length) = law_of_cosines(center[0], center[1], globalX, globalY, prevX, prevY)
-    #print("The Arc length is %f") % a
     #Slope is just really globalJ/globalY
     return (center, radius, angle, length)
+
 def arc_patch(center, radius, theta1, theta2, ax=None, resolution=50, **kwargs):
     # make sure ax is not empty
     if ax is None:
@@ -308,17 +291,10 @@ def parseFile(fileName):
         # Attaching 3D axis to the figure
         fig = plt.figure()
         ax = p3.Axes3D(fig)
-        #line_ani = animation.FuncAnimation(fig, update_lines, 25, fargs=(data, lines),
-         #                          interval=50, blit=False)
-        #background = fig.canvas.copy_from_bbox(ax.bbox)
         for line in file:
             lineNum  += 1
             gcode     = line.upper().split()
             parseLine(gcode)
-            #print("%d. Line: %s\n") % (lineNum, gcode)
-            #print("FeedRate: %d\t Spindle Speed: %d\t Tool Number:%d MovementType: %s\n") % (feedRate, spindleSpeed, toolNum, movementType)
-            #print("(%f,%f) (I,J)\n") %(globalI, globalJ)
-            #print("Location: (%f,%f,%f)\n") % (globalX, globalY, globalZ)
             time       = 0
             local_feed = feedRate
             distance   = 0
@@ -326,27 +302,20 @@ def parseFile(fileName):
                 center, radius, angle, length = drawArc()
                 distance                      = length
                 arcs.append(((globalX,globalY,globalZ),(prevX,prevY,prevZ),radius))
-                #arc_patch(center, radius, 180, 90, ax=ax, fill = 'false',color='green')
             elif(movementType == 'G03' or movementType == 'G3'):
                 center, radius, angle, length = drawArc()
                 distance                      = length
                 arcs.append(((prevX,prevY,prevZ),(globalX, globalY, globalZ), radius))
-                #arc1 = mpatches.Arc(center, math.fabs(prevX-globalX), math.fabs(prevY-globalY), math.degrees(angle), 90, 180, color='pink')
-                #ax.add_patch(arc1)
             elif(movementType == 'G00' or movementType == 'G0'):
                 distance = calcDistance(globalX, globalY, globalZ, prevX, prevY, prevZ)
                 lines.append(((prevX, prevY, prevZ), (globalX, globalY, globalZ)))
                 local_feed = rapid
                 line1 = [(prevX, prevY), (globalX, globalY)]
                 (line1_xs, line1_ys) = zip(*line1)
-                #ax.add_line(plt.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
-                #fig.canvas.restore_region(background)
                 if(first_time == 0):
                     ax.plot((prevX, globalX), (prevY, globalY), (prevZ, globalZ))
                 else:
                     first_time -= 1
-                # fill in the axes rectangle
-                #fig.canvas.blit(ax.bbox)
             elif(movementType == 'G01' or movementType == 'G1'):
                 distance = calcDistance(globalX, globalY, globalZ, prevX, prevY, prevZ)
                 lines.append(((prevX, prevY, prevZ), (globalX, globalY, globalZ)))
@@ -356,28 +325,15 @@ def parseFile(fileName):
                     ax.plot((prevX, globalX), (prevY, globalY), (prevZ, globalZ))
                 else:
                     first_time -= 1
-                #ax.add_line(plt.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
             if(prevTool != toolNum):
                 total_time += toolChangeTime
                 prevTool    = toolNum
             time  = calcTime(distance, local_feed)
-            #print("Distance: %f Time: %f") % (distance, time)
             total_time += time
             prevX = globalX
             prevY = globalY
             prevZ = globalZ
-            #plt.draw()
-            #plt.pause(0.0001)
-            #plt.show()
 
-
-
-        #line_collection = [Gen_Line(s,e) for s,e in lines]
-
-        # Creating fifty line objects.
-        # NOTE: Can't pass empty arrays into 3d version of plot()
-        #lines2 = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in line_collection]
-        # ax.set_xlim3d([0.0, 1.0])
         ax.set_xlabel('X')
         # ax.set_ylim3d([0.0, 1.0])
         ax.set_ylabel('Y')
@@ -385,43 +341,7 @@ def parseFile(fileName):
         ax.set_zlabel('Z')
         ax.set_title('3D Test')
 
-        #line_animation = animation.FuncAnimation(fig, update_lines, 25, fargs=(line_collection, lines2), interval=50, blit=False, repeat = False)
-
         plt.show()
-        #plt.pause(0.0001)
-
-
-
-
-
-
-
-        # for ((x1, y1, z1), (x2, y2, z2)), index in zip(lines, numpy.arange(len(lines))):
-        #     lines[:, index] =
-
-
-
-
-        #print("Total Time: %f") % total_time
-        #print("Arcs:")
-        '''
-        for p in arcs:
-            s, e, r      = p
-            (x1, y1, z1) = s
-            (x2, y2, z2) = e
-            print("Start: (%f, %f, %f)\t\t|\t\t End:(%f, %f, %f)\t\t|\t\tRadius:%f") % (x1, y1, z1, x2, y2, z2, r)
-        print("Lines:")
-        for w in lines:
-            s, e = w
-            (x1, y1, z1) = s
-            (x2, y2, z2) = e
-            print("Start: (%f, %f, %f)\t\t|\t\t End:(%f, %f, %f)") % (x1, y1, z1, x2, y2, z2)
-        '''
-        # plt.autoscale(True, True, True)
-        # plt.axis('scaled')
-        # plt.show()
-            #CCW Arc: I,J are centerpoints, globalX, globalY are endpoints
-#matplotlib.interactive(True)
 parseFile(str(sys.argv[1]))
 
 #turtle.done()
